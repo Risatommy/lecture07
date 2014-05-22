754e730be8937ef24982b35add075f310278e379
Risatommy <risa555@gmail.com>
2014-05-22 12:29:02 +0900

    テキストの入力欄を８つに増加

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,14 @@
 var photoListElement = document.querySelector("#photos");
 
 var photoList = [
-    {src: "img/200705.png", title: "7月"},
-    {src: "img/200806.png", title: "8月"},
-	  {src: "img/200902.png", title: "9月"},
-	  {src: "img/201008.png", title: "もう1回8月"},
-    {src: "img/front.png", title: "前"},
-    {src:	"img/side.png", title: "横"}
+    {src: "img/sb1.png", title: "7月"},
+    {src: "img/sb2.png", title: "8月"},
+	  {src: "img/sb.png", title: "9月"},
+	  {src: "img/sb3.png", title: "もう1回8月"},
+    {src: "img/kuma.png", title: "前"},
+    {src:	"img/sb5.png", title: "横"}
+    //画像が表示される
+    
 ];
 
 var isReady = function(){
